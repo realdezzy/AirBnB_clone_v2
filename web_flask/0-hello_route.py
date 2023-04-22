21d2b68f98ee67bfe0c0d6bd7832331561332fdd
realdezzy <realdezzy1@gmail.com>
2023-04-22 16:39:39 -0700

    Style check

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
+"""Flask Module"""
 from flask import Flask
 from os import environ
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Entry route """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
     environ["FLASK_APP"] = __file__
     app.run(host='0.0.0.0', port=5000)
-
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,17 @@
+#!/usr/bin/python3
+"""Flask Module"""
+
 from flask import Flask
 from os import environ
 app = Flask(__name__)
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_flask():
     """ Hbnb route """
     return "HBNB!"
 
+
 if __name__ == '__main__':
     environ["FLASK_APP"] = __file__
     app.run(host='0.0.0.0', port=5000)
-
